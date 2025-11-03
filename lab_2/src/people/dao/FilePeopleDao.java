9499f596d6b9144dd7910a6c8a3185d2fcd556e2
reshetova.sa <reshetova.sa@dozen.mephi.ru>
2025-11-03 21:03:22 +0300

    FilePeopleDao.java done

--- lab_2/src/people/dao/FilePeopleDao.java
+++ lab_2/src/people/dao/FilePeopleDao.java
@@ -20,7 +20,7 @@ public class FilePeopleDao implements PeopleDao {
             Validation.validate(p);
 
             if (p.getId() == null || p.getId().trim().isEmpty()) {
-                throw new IllegalArgumentException("ID is required");
+                throw new IllegalArgumentException("ID is invalid");
             }
 
             Path targetFile = storageDir.resolve(id + ".json");
@@ -38,14 +38,62 @@ public class FilePeopleDao implements PeopleDao {
     }
     @Override
     public Optional<Person> findById(String id) {
+        try {
+            if (id  == null || id.trim().isEmpty()) {
+                return Optional.empty();
+            }
 
+            Path filePath = storageDir.resolve(id + ".json");
+            if (!Files.exists(filePath)) {
+                return Optional.empty();
+            }
+
+            String json = Files.readString(filePath);
+
+            Person p = JsonUtil.fromJson();
+
+            return Optional.of(p);
+        } catch (Esception e) {
+            throw new RuntimeException("Failed to find person with id: " + id);
+            return Optional.empty();
+        }
     }
     @Override
     public void update(Person p) {
+        try {
+            if (p == null) {
+                throw new IllegalArgumentException("Person is null");
+            }
+            String id = p.getId();
+            if  (id == null || id.trim().isEmpty()) {
+                throw new IllegalArgumentException("ID is invalid");
+            }
 
+            Path filePath = storageDir.resolve(id + ".json");
+            if (!Files.exists(filePath)) {
+                throw new RuntimeException("Failed to find person with id: " + id);
+            }
+
+            create(p);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to update person: " + e.getMessage());
+        }
     }
     @Override
     public boolean delete(String id) {
+        try {
+            if ( if id == null || id.trim().isEmpty()){
+                return false;
+            }
 
+            Path filePath = storageDir.resolve(id + ".json");
+            if (!Files.exists(filePath)) {
+                return false;
+            }
+
+            return Files.deleteIfExists(filePath);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete person: " + e.getMessage());
+        }
     }
 }
